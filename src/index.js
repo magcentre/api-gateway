1b1c1f9df6a85f5abdeec4264debf7de9e06ee5e
Prasanna Mangoli <mangoli.prasanna05@gmail.com>
2021-11-27 16:39:37 +0530

    initial commit (#1)
    Co-authored-by: Prasanna Mangoli <Prasanna Mangoli>

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const gateway = require('fast-gateway')
+
+const logger = require("@magcentre/logger-helper");
+
+const serviceRoutes = require('./route.generator');
+
+const service = gateway({
+    middlewares: [
+        require('./middleware/logger'),
+    ],
+    routes: serviceRoutes,
+})
+
+service.start(process.env.PORT).then(() => {
+    logger.info(`Gateway Service started at ${process.env.PORT}`,)
+}).catch(e => logger.error(e))
